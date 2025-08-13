13c000e3c904b6984cfa9d51e63ab2e21f5eb513
SARTHAK SHISHODIA <122453870+sarthakshishodia20@users.noreply.github.com>
2025-08-13 06:57:52 +0530

    Create EXCELSHEETCOULMNTITLE.java

---
+++ EXCELSHEETCOULMNTITLE.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int result=0;
+        for(int i=0;i<columnTitle.length();i++){
+            char ch=columnTitle.charAt(i);
+            int making=ch-'A'+1;
+            result=result*26+making;
+        }
+        return result;
+    }
+}
